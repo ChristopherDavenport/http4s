9d3eb3aab8254edcf61e998d3b06946ba5ce8347
Christopher Davenport <ChristopherDavenport@outlook.com>
2017-06-22 12:24:15 -0400

    CI Test Playground Modules

--- bin/test.sh
+++ bin/test.sh
@@ -3,7 +3,7 @@
 . $TRAVIS_BUILD_DIR/bin/setup.sh
 
 start_watchdog 13m
-sbt $SBT_EXTRA_OPTS ++$TRAVIS_SCALA_VERSION "test"
+sbt $SBT_EXTRA_OPTS ++$TRAVIS_SCALA_VERSION "${HTTP4S_MODULE}test"
 exitCode=$?
 stop_watchdog
 exit ${exitCode}
